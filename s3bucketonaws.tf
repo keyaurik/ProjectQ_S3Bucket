d1da8c36f8143456952c159da3656fc85f6c86d9
Keyauri Kendrick <keyauri.kendrick@gmail.com>
2020-05-15 17:08:25 -0400

    Update s3bucketonaws.tf

--- s3bucketonaws.tf
+++ s3bucketonaws.tf
@@ -7,3 +7,15 @@ resource "aws_s3_bucket" "b" {
     Environment = "Dev"
   }
 }
+{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
+      "Sid":"PublicRead",
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::examplebucket/*"]
+    }
+  ]
+}
