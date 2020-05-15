601c99186cc0ad34f32323bebb5a1f64da8e503e
Keyauri Kendrick <keyauri.kendrick@gmail.com>
2020-05-15 17:27:44 -0400

    Update s3bucketonaws.tf

--- s3bucketonaws.tf
+++ s3bucketonaws.tf
@@ -1,14 +1,9 @@
-resource "aws_s3_bucket" "log_bucket" {
-  bucket = "projectqkdk_log_bucket"
-  acl    = "log-delivery-write"
-}
-
 resource "aws_s3_bucket" "bucket" {
-  bucket = "projectqkdk"
+  bucket = "projectqtestbucket"
   acl    = "private"
 
-  logging {
-    target_bucket = "${aws_s3_bucket.log_bucket.id}"
-    target_prefix = "log/"
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
