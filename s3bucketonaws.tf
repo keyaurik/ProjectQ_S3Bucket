ce87d98f3197b3cf82a55a1d3278fe2645329eef
Keyauri Kendrick <keyauri.kendrick@gmail.com>
2020-05-15 16:34:23 -0400

    Update s3bucketonaws.tf

--- s3bucketonaws.tf
+++ s3bucketonaws.tf
@@ -7,3 +7,13 @@ resource "aws_s3_bucket" "bucket" {
     Environment = "test"
   }
 }
+
+provider "aws" {
+  version = "~> 2.0"
+  region  = "${var.aws_region}"
+}
+
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-east-1"
+}
